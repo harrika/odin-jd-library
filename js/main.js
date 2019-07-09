3f2be0bf9f6118735c1859953ff2227ed630dd66
henry kirya <henry.kirya@gmail.com>
2019-07-09 23:00:15 +0300

    fixed status change

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 		var lib = [];
+		var sti = ["Read", "Not Read"];
 		iid= 0
 		document.querySelector('#adder').addEventListener('click', addtolib);
 
+		//seetup hte Book object
 		function Book(title, author, pages, status, id) {
 		    this.id = id;
 		    this.title = title;
 		    this.author = author;
 		    this.pages = pages;
 		    this.status = status;
+		}
 
-		    togstat = function(s) {
-		    	return  (s == 'Read') ? "Not Read" : "read";
-			}
+		//method to toggle the book read status
+		Book.prototype.togglestat = function() {
+			if (this.status == "Read") {
+				(this.status = "Not Read")
+			} else {
+				this.status = "Read";
+			}	
+			//console.log(this.status);
+			cleardrow(lib); 
 		}
 
 		function addtolib(e) {
@@ -26,7 +35,7 @@
 		    cleardrow(lib); 		    
 		    }
 
-		 //Adds book rows to DOM  after clearing
+		 //The function clears hten adds book rows to the DOM
 		 function cleardrow(lb) {
 		 	delros();
 		 	lb.forEach(bb => {
@@ -48,25 +57,7 @@
 		 	lib.splice(pp,1);
 		 	cleardrow(lib);		  	
 		  }
-
-		  //togling the book status
-		  function steto() {
-		  	alert(this.iid);
-		  	// dd = this.iid;
-		  	// const dabuk = lib.find(book => book['iid'] === 1);
-		  	// console.log(dabuk.iid);	
-
-
-		  	let debuk = iid => {
-		  	  return iid == 1;
-		  	}
-
-		  	console.log(lib.find(debuk));
-
-		  	
-		  	cleardrow(lib);
-		  }
-
+		
 		 //adding a book row to the DOM
 		 function addbkro(bk) {
 				var tebody = document.querySelector('#tb');	
@@ -76,10 +67,42 @@
 		 			<td>${bk.title}</td>
 		 			<td>${bk.author}</td>
 		 			<td>${bk.pages}</td>
-		 			<td>${bk.status}</td>
-		<td><input type="button" onclick="steto()" value="${bk.status}"> </td>
+		<td><input type="button" onclick="stati(${bk.id})" value="${bk.status}"> </td>
 		<td><input type="button" onclick="dell()" value="delete"> </td>
 		 		`;
 		 	    tebody.append(terow);
 		 }
 
+		 // window.onload=function(){
+		 // 	var statbtn = document.querySelector('#stat');
+		 // 	statbtn.addEventListener('click', function(e) {
+		 // 	var target = e.target;
+		 // 	console.log(target)
+		 // 	});
+		 // }
+
+		 function stati(d) {
+		 	// alert(d);
+		 	const i = lib.findIndex(function(bk, i) {
+		 		return bk.id === d;
+		 	})
+		 	const buk = lib[i];
+		 	buk.togglestat();		 	
+
+		 	// alert(buk.status);
+		 	// if buk.status == "Read" {
+		 	// 	buk.togglestat(0);
+		 	// } else {
+		 	// 	buk.togglestat(0);
+		 	// }
+		 
+
+		 }
+
+// ===================================================
+// how to procedd
+// get bk.id from click
+// find book with such id in library  lib
+// delete bk
+// or change status of book
+// ======================================================
