7910167103f5fbe2b029a617c659ef7473badcf2
henry kirya <henry.kirya@gmail.com>
2019-07-09 16:56:18 +0300

    status

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 		var lib = [];
+		iid= 0
 		document.querySelector('#adder').addEventListener('click', addtolib);
 
 		function Book(title, author, pages, status, id) {
@@ -19,19 +20,21 @@
 		    var aauthor = document.querySelector('#authorIn').value;
 		    var ppages = document.querySelector('#pagesIn').value;
 		    var sstatus = document.querySelector('#redIn').value; 
-		   
-		    var book = new Book(ttitle, aauthor, ppages, sstatus);
+		    iid += 1;		   
+		    var book = new Book(ttitle, aauthor, ppages, sstatus, iid);
 		    lib.push(book);
 		    cleardrow(lib); 		    
 		    }
 
+		 //Adds book rows to DOM  after clearing
 		 function cleardrow(lb) {
 		 	delros();
 		 	lb.forEach(bb => {
 		 		addbkro(bb);
 		 	});
 		 }
-	
+
+		 //clears rows from table DOM	
 		 function delros() {
 			var tebo = document.querySelector('#dtable');	
 		 	while(tebo.rows.length > 0) {
@@ -39,13 +42,32 @@
 		 	}
 		 }
 
-		  function dell(bkid) {
-		  	var pp = lib.indexOf(this);
+		  //removes a book fro hte library
+		  function dell() {
+		  	var pp = lib.indexOf(this);	
 		 	lib.splice(pp,1);
 		 	cleardrow(lib);		  	
 		  }
 
+		  //togling the book status
+		  function steto() {
+		  	alert(this.iid);
+		  	// dd = this.iid;
+		  	// const dabuk = lib.find(book => book['iid'] === 1);
+		  	// console.log(dabuk.iid);	
 
+
+		  	let debuk = iid => {
+		  	  return iid == 1;
+		  	}
+
+		  	console.log(lib.find(debuk));
+
+		  	
+		  	cleardrow(lib);
+		  }
+
+		 //adding a book row to the DOM
 		 function addbkro(bk) {
 				var tebody = document.querySelector('#tb');	
 		 	   	var terow = document.createElement('tr');
@@ -55,7 +77,8 @@
 		 			<td>${bk.author}</td>
 		 			<td>${bk.pages}</td>
 		 			<td>${bk.status}</td>
-	 	        <td><input type="button" onclick="dell()" value="delete"> </td>
+		<td><input type="button" onclick="steto()" value="${bk.status}"> </td>
+		<td><input type="button" onclick="dell()" value="delete"> </td>
 		 		`;
 		 	    tebody.append(terow);
 		 }
